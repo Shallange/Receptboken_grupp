1b0e78e6fc5f89975a571d8386bbe881cc62b347
Shallange <jimmy.kroneld@hotmail.com>
2026-03-25 13:24:43 +0100

    Initial Commit

---
+++ Receptboken_grupp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Receptboken_grupp
+//
+//  Created by Jimmy kroneld on 2026-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Receptboken_grupp/Receptboken_gruppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receptboken_gruppApp.swift
+//  Receptboken_grupp
+//
+//  Created by Jimmy kroneld on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct Receptboken_gruppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
